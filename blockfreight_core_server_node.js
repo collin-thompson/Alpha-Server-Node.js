34c5a04812f22a970a76094be3a213b53ba236f5
julian-smith-code <julian.smith@webdeli.com.au>
2016-04-30 20:30:42 +1000

    Add first commit

---
+++ blockfreight_core_server_node.js
@@ -0,0 +1,54 @@
+// File: blockfreight_core_server_node.js
+// Summary: NodeJS application code for Blockfreight™ | The blockchain of global freight.
+// License: GPL Version 3
+// Author: Blockfreight Pty Ltd [Australia]
+// Web: https://www.blockfreight.com
+// Support: <support@blockfreight.com>
+
+// =================================================================================================================================================
+// =================================================================================================================================================
+//                                                                                                                                                  
+// BBBBBBBBBBBb     lll                                kkk             ffff                         iii                  hhh            ttt         
+// BBBB``````BBBB   lll                                kkk            fff                           ```                  hhh            ttt         
+// BBBB      BBBB   lll      oooooo        ccccccc     kkk    kkkk  fffffff  rrr  rrr    eeeee      iii     gggggg ggg   hhh  hhhhh   tttttttt      
+// BBBBBBBBBBBB     lll    ooo    oooo    ccc    ccc   kkk   kkk    fffffff  rrrrrrrr eee    eeee   iii   gggg   ggggg   hhhh   hhhh  tttttttt      
+// BBBBBBBBBBBBBB   lll   ooo      ooo   ccc           kkkkkkk        fff    rrrr    eeeeeeeeeeeee  iii  gggg      ggg   hhh     hhh    ttt         
+// BBBB       BBB   lll   ooo      ooo   ccc           kkkk kkkk      fff    rrr     eeeeeeeeeeeee  iii   ggg      ggg   hhh     hhh    ttt         
+// BBBB      BBBB   lll   oooo    oooo   cccc    ccc   kkk   kkkk     fff    rrr      eee      eee  iii    ggg    gggg   hhh     hhh    tttt    ....
+// BBBBBBBBBBBBB    lll     oooooooo       ccccccc     kkk     kkkk   fff    rrr       eeeeeeeee    iii     gggggg ggg   hhh     hhh     ttttt  ....
+//                                                                                                        ggg      ggg                              
+//   Blockfreight™ | The blockchain of global freight.                                                      ggggggggg                               
+//                                                                                                                                                  
+// =================================================================================================================================================
+// =================================================================================================================================================
+
+// First commit.
+
+// =================================================
+// Blockfreight™ | The blockchain of global freight.
+// =================================================
+
+// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+// BBBBBBB                    BBBBBBBBBBBBBBBBBBB
+// BBBBBBB                       BBBBBBBBBBBBBBBB
+// BBBBBBB                        BBBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBBBB        BBBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBBBB        BBBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBB         BBBBBBBBBBBBBBBB
+// BBBBBBB                     BBBBBBBBBBBBBBBBBB
+// BBBBBBB                        BBBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBBBBB        BBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBBBBBB       BBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBBBBB        BBBBBBBBBBBBBB
+// BBBBBBB       BBBBBBBBB        BBB       BBBBB
+// BBBBBBB                       BBBB       BBBBB
+// BBBBBBB                    BBBBBBB       BBBBB
+// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+// BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB
+
+// ==================================================
+// Blockfreight™ | The blockchain for global freight.
+// ==================================================
